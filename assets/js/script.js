a76678bf3010b853fe5de952188848e5ea77916f
Yamila Lemos <yamilatatiana.lemos@nttdata.com>
2022-07-20 09:59:53 -0300

    "Polished UI, added details to the Product card"

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,31 @@ window.onclick = function (event) {
     }
   }
 };
+
+fetch('carsJSON.json')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
+
+/* var grupoA = [{"Name": "Kia Rio or similar",
+"AirConditionInd": "true",
+"Code": "ECAR",
+"TransmissionType": "Automatic",
+"VehType": "1",
+"VehClass": "3",
+"VehGroup": "A",
+"PictureURL": "https://www.avis.com/content/dam/cars/l/2020/kia/2020-kia-rio-s-sedan-grey_featured.png",
+"Features": "Smoke Free|4 Doors|5 Seats|Radio|Air Bag|Air Conditioning|Automatic|Holds 1 large, 1 small Suitcases|27-35 mpg|image=2020-kia-rio-s-sedan-grey.png|thumb=2020-kia-rio-s-sedan-grey.png|cargroup=A|category=Economy",
+"Features2": {
+    "doors": "4",
+    "seats": "5",
+    "air": "Air Conditioning",
+    "transmition": "Automatic",
+    "largeSuitcase": "1",
+    "smallSuitcase": "1",
+    "thumb": "2020-kia-rio-s-sedan-grey.png",
+    "category": "Economy"}];
+
+var a = carsJSON.stringify(grupoA);
+console.log(grupoA) */
